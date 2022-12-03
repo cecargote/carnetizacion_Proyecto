766497a1275ee983d770f06ddbae01690c250f69
cecargote <carlos.argoteq97@gmail.com>
2022-12-03 00:23:45 -0500

    cantidad de trabajadores

--- carnetizacion/app/webapp/admin/router_admin.py
+++ carnetizacion/app/webapp/admin/router_admin.py
@@ -12,7 +12,7 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from core.config import settings
 import requests
-
+import json
 templates = Jinja2Templates(directory="templates")
 
 router = APIRouter()
@@ -23,11 +23,15 @@ async def admin(request: Request,
 db: Session = Depends(get_db),
 ):
      print("Cargando Admin")
+
+     count= repr(cantidad_trabajadores())
+     print ("count "+count)
      token = request.cookies.get("access_token")
      scheme, param = get_authorization_scheme_param(token)  # scheme will hold "Bearer" and param will hold actual token value
+     
      responses = templates.TemplateResponse(
-            "admin/admin_template.html", {"request": request}
-        )
+            "admin/admin_template.html", {"request": request, "count": count})
+     
      current_user: Usuario = get_current_user_from_token(
             param, db
         ) 
@@ -38,14 +42,24 @@ db: Session = Depends(get_db),
      print("El usuario actual no es admin -> Login")  
      return responses.RedirectResponse("login", status_code=status.HTTP_302_FOUND)
 
-def cantidad_trabajadores():
-    url = settings.API_AUDIENCE + "workers"
-    
-    area = "OU=Facultad de Ingenieria Mecanica,DC=cujae,DC=edu,DC=cu"
-    data = { "area": area}
-    responseURL = requests.post(url, auth=("fpicayo.sec", "Sigenu_sec_*2014*"),json=data)
-    print ("url response "+responseURL.url+ " status: "+responseURL.status_code)
-    
-    print(responseURL.text)
+def cantidad_trabajadores_totales():
+ 
+    reqUrl = "https://sigenu.cujae.edu.cu/sigenu-ldap-cujae/ldap/workers?area=OU=Facultad de Ingenieria Mecanica,DC=cujae,DC=edu,DC=cu"
 
-    return None
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Authorization": "Basic ZGlzZXJ0aWMubGRhcDpkaXNlcnRpYyoyMDIyKmxkYXA=",
+    "Content-Type": "application/json" 
+    }
+
+    payload = json.dumps({
+    "area": "OU=Facultad de Ingenieria Mecanica,DC=cujae,DC=edu,DC=cu"
+    })
+
+    response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
+    
+    responseArray = response.json()
+    count =len(responseArray)
+    print("cantidad total de trabajadores ",count)
+    return count

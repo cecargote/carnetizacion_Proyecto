2042443a41c70d9ff1d237cbc1a9b0b3e835f3ce
cecargote <carlos.argoteq97@gmail.com>
2022-12-03 17:23:43 -0500

    logout delete

--- carnetizacion/app/webapp/admin/router_admin.py
+++ carnetizacion/app/webapp/admin/router_admin.py
@@ -1,8 +1,9 @@
 import jwt
 from api.endpoints.router_login import get_current_user_from_token
+from db.repository.usuario import update_state_usuario_by_id_logout
 from db.models.usuario import Usuario
 from db.session import get_db
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi import Request
 from fastapi import responses
 from fastapi import status
@@ -17,32 +18,30 @@ templates = Jinja2Templates(directory="templates")
 
 router = APIRouter()
 
-
 @router.get("/admin")
 async def admin(request: Request, 
 db: Session = Depends(get_db),
 ):
      print("Cargando Admin")
-
-     count= repr(cantidad_trabajadores())
-     print ("count "+count)
      token = request.cookies.get("access_token")
      scheme, param = get_authorization_scheme_param(token)  # scheme will hold "Bearer" and param will hold actual token value
-     
+     count= repr(cantidad_trabajadores_and_estudiantes())
      responses = templates.TemplateResponse(
             "admin/admin_template.html", {"request": request, "count": count})
+     try:
+        current_user: Usuario = get_current_user_from_token(param, db)
+     except HTTPException:
+        print("Error al cargar el usuario, sera enviado al LOGIN")
+        return  responses.RedirectResponse("login", status_code=status.HTTP_401_UNAUTHORIZED)
      
-     current_user: Usuario = get_current_user_from_token(
-            param, db
-        ) 
-     print (current_user.nombre_usuario)
      if (current_user.rol_usuario == "Administrador" or current_user.rol_usuario == "SuperAdmin"):
-            print("Admin - > User: "+current_user.nombre_usuario)
+            print("Admin - > User: "+current_user.nombre_usuario)           
+            
             return responses
      print("El usuario actual no es admin -> Login")  
      return responses.RedirectResponse("login", status_code=status.HTTP_302_FOUND)
 
-def cantidad_trabajadores_totales():
+def cantidad_trabajadores_and_estudiantes():
  
     reqUrl = "https://sigenu.cujae.edu.cu/sigenu-ldap-cujae/ldap/persons?area=OU=DG de ICI,OU=Area Central,DC=cujae,DC=edu,DC=cu"
     headersList = {
@@ -59,3 +58,4 @@ def cantidad_trabajadores_totales():
     count =len(responseArray)
     print("cantidad total de trabajadores ",count)
     return count
+

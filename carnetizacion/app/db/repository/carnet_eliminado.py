dd018120196cb929c15a05237b37d7ef6a9f333f
cecargote <carlos.argoteq97@gmail.com>
2022-12-19 22:16:07 -0500

    good

--- carnetizacion/app/db/repository/carnet_eliminado.py
+++ carnetizacion/app/db/repository/carnet_eliminado.py
@@ -3,9 +3,10 @@ from sqlalchemy import desc
 
 from schemas.carnet_eliminado import CarnetEliminadoCreate
 from db.models.carnet_eliminado import CarnetEliminado
-
+from 
 
 def create_new_carnet_eliminado(carnet_eliminado: CarnetEliminadoCreate,db: Session,person_ci:int):
+
     carnet_eliminado_object = CarnetEliminado(**carnet_eliminado.dict(),person_ci=person_ci)
     db.add(carnet_eliminado_object)
     db.commit()

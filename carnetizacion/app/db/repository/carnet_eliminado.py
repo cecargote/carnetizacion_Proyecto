c7ce6e1d129d5254a99984b4e250c1804f7a003e
cecargote <carlos.argoteq97@gmail.com>
2023-02-01 22:22:37 -0500

    ultimos cambios

--- carnetizacion/app/db/repository/carnet_eliminado.py
+++ carnetizacion/app/db/repository/carnet_eliminado.py
@@ -1,9 +1,8 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import desc
-
 from schemas.carnet_eliminado import CarnetEliminadoCreate
 from db.models.carnet_eliminado import CarnetEliminado
-from 
+
 
 def create_new_carnet_eliminado(carnet_eliminado: CarnetEliminadoCreate,db: Session,person_ci:int):
 

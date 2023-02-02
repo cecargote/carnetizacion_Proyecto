a6b89f801221e0eccaf87f9dce801f5938895629
cecargote <carlos.argoteq97@gmail.com>
2023-02-01 23:02:07 -0500

    ultimos cambios

--- carnetizacion/app/webapp/crear_carnet/router_crear_carnet.py
+++ carnetizacion/app/webapp/crear_carnet/router_crear_carnet.py
@@ -53,7 +53,55 @@ router = APIRouter()
 
 ##EndPoints APP
 # router.include_router(userRouter, prefix="/user", tags=["Users"])
+def buscar_Tipo_Estudiante_carnet(ci: str):
+    reqUrl = "https://sigenu.cujae.edu.cu/sigenu-rest/student/fileStudent/getStudentAllData/"+ci
 
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Authorization": "Basic ZGlzZXJ0aWMud3Muc2lnZW51OmRpc2VydGljLndzKjIwMTUq",
+    "Content-Type": "application/json" 
+    }
+
+    payload = json.dumps("")
+
+    response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
+
+
+
+    result = json.loads(response.text)
+    try:
+        tipo =result[0]["docentData"]['studentType'] 
+
+        print("tipo de estudiante",tipo)
+    except Exception as e:
+        print("no se encontro el estudiante")
+        print(e)
+
+    return tipo
+def buscar_consejo_universitario(ci:str, name : str, last_name):
+    archivo = 'C:/Users/Carlos Laptop/Downloads/datos2.xlsx'
+    hoja = pandas.read_excel(archivo,engine="openpyxl")
+    list = hoja
+    last_name_temp=""
+    name_temp= ""
+    ci_temp = ""
+    cargo_temp=""
+    i=0
+    size = len(list)
+    while i< size:
+        last_name_temp= list.iloc[i,3]
+        name_temp= list.iloc[i,2]
+        ci_temp = list.iloc[i,1]
+        ci_temp = ""+str(ci_temp)
+
+        if(name_temp==name and last_name_temp ==last_name or ci_temp==ci ):
+            cargo_temp= list.iloc[i,6]
+            print("se encontro un cargo para esta persona")
+            print(i, " ",ci_temp," " ,name_temp," ",last_name_temp," cargo: ",cargo_temp)
+        i+=1
+
+    return cargo_temp
 def buscarTrabajdor_and_Estudiante(ci: str,area: str):
 
     reqUrl = "https://sigenu.cujae.edu.cu/sigenu-ldap-cujae/ldap/search-all"
@@ -118,8 +166,9 @@ async def crear_carnet(area,ci, request: Request, db: Session = Depends(get_db))
     try:
         list_tipo_motivos = list_motivos(db=db)
         carnet_user = get_carnet_by_person(ci, db)
-        print(carnet_user)
+        print("Carnet Anterior ",carnet_user)
         person_carnet = retreive_person(ci,db)
+        print("Persona Anterior",person_carnet)
         print(person_carnet)
         token = request.cookies.get("access_token")
         scheme, param = get_authorization_scheme_param(token)
@@ -127,59 +176,42 @@ async def crear_carnet(area,ci, request: Request, db: Session = Depends(get_db))
 
         usuario= buscarTrabajdor_and_Estudiante(ci,areatemp)
        
-        if usuario != None :         
+        if usuario != None :
+            rol =""         
             user= usuario[0]
+            
+            if(user["personType"] == "Student"):
+                rol= buscar_Tipo_Estudiante_carnet(ci)
+                print("rol ",rol)
+            elif user["personType"] == "Worker":
+                if user["personTeacher"] == "TRUE":
+                    rol= "Docente"         
+                elif user["personTeacher"] == "FALSE":
+                    rol= "No Docente"
 
-            responseB= templates.TemplateResponse("general_pages/crear_carnet.html",{"request": request, "list_tipo_motivos": list_tipo_motivos, 'user':user, 'area':area, 'carnet_user':carnet_user, 'person_carnet':person_carnet})    
+
+            name_temp= user["name"]
+            last_name = user["surname"] + " "+ user["lastname"]
+            found = buscar_consejo_universitario(ci,name_temp,last_name)
+            
+            if found !="":
+                rol = found
+
+
+            print("rol ", rol)
+            responseB= templates.TemplateResponse("general_pages/crear_carnet.html",{"request": request,
+             "list_tipo_motivos": list_tipo_motivos,
+              'user':user,
+               'area':area,
+               'carnet_user':carnet_user,
+                'person_carnet':person_carnet,
+                'rol': rol})    
+             
                      
             current_user: Usuario = get_current_user_from_token(param, db)
            
             if ( current_user.rol_usuario == "Carnetizador" or current_user.rol_usuario == "SuperAdmin"):
 
-
-            
-
-            # GENERAR QR CODE
-            # formato de la info
-            # N: Nombre y Apellidos, CI: ci, A: area, F:folio
-            # qrtxt = (
-            #     "aqui va el texto del qr"
-            # )  # "N:" + test[posicion]["nombreCrearCarnet"] + " CI:" + test[posicion]["ciCrearrear"] + " A:" + test[posicion]["areaBuscarPersona"] + " F:" + test[posicion]["folioCrearCarnet"]
-            # qr = qrcode.QRCode(
-            #     version=1,
-            #     error_correction=qrcode.constants.ERROR_CORRECT_L,
-            #     box_size=10,
-            #     border=4,
-            # )
-            # qr.add_data(qrtxt)
-            # qr.make(fit=True)
-            # img = qr.make_image(fill_color="black", back_color="white")
-            # type(img)
-            # img.save("QRCode.png")
-
-            # # GENERAR BARCODE
-            # # bc = test[posicion]["ciCrearCarnet"]
-            # # image = treepoem.generate_barcode(
-            #     # barcode_type="code128",  # One of the BWIPP supported codes.
-            #     # barcode_type="qrcode",
-            #     # One of the BWIPP supported codes.
-            #     # barcode_type="interleaved2of5",  # One of the BWIPP supported codes.
-            #     # barcode_type="code128",  # One of the BWIPP supported codes.
-            #     #    # barcode_type="isbn",  # One of the BWIPP supported codes.
-            #     #    # data="978-3-16-148410-0",
-            #     #   barcode_type="code128",  # One of the BWIPP supported codes.
-            #     #   barcode_type="micropdf417",  # One of the BWIPP supported codes.
-            #     #   barcode_type="ean13",  # One of the BWIPP supported codes.
-            #     # data=bc,
-            # # )
-            # # image.convert("1").save("BCCode.png")
-
-            # # return templates.TemplateResponse(
-            # # "general_pages/crear_carnet.html", {"request": request, "list_tipo_motivos": list_tipo_motivos, "test":test, "posicion":posicion, "tamano":tamano, "qrtxt":qrtxt, "img":img}
-            # # )
-            # # else:
-            # # return responses.RedirectResponse("", status_code=status.HTTP_302_FOUND)
-            # # endif
                 return responseB
     except Exception as e:
         print(e)
@@ -204,10 +236,24 @@ async def crear_carnet_post(area,ci,request:Request, db: Session = Depends(get_d
             
            
             if(user["personType"] == "Student"):
+                form.rol = buscar_Tipo_Estudiante_carnet(form.ci)
                 form.annoEstudiantePersona= user['studentYear']
                 form.tipoPersona = "Estudiante"
             else:
                 form.tipoPersona = "Trabajador"
+                if user["personTeacher"] == "TRUE":
+                    form.rol = "Docente"         
+                elif user["personTeacher"] == "FALSE":
+                    form.rol = "No Docente"
+            
+            name_temp= user["name"]
+            last_name = user["surname"] + " "+ user["lastname"]
+
+            found = buscar_consejo_universitario(ci,name_temp,last_name)
+
+            if found !="":
+                print(found)
+                form.rol = found 
             
             form.nombre= user['name']+" "+user['lastname']+" "+user['surname']
             
@@ -223,17 +269,28 @@ async def crear_carnet_post(area,ci,request:Request, db: Session = Depends(get_d
             print("===========Datos del usuario======================")
             print("ci: ",form.ci)
             #print("folio ",form.folio)
-            print("folio desactivo ",form.folio_desactivo)
+            if carnet_anterior != None:
+                print("folio desactivo ",carnet_anterior.folio)
+            else:
+                print("folio desactivo: No existe folio anterior")
             print("area ",form.area)
-            print("area anterior ",form.area_anterior)
+            if person != None:
+                print("area anterior ",person.area)
+            else:
+                print("area anterior: "+"No existe Area anteriror")
             print("comprobante motivo ",form.comprobante_motivo)
             print("estado ",form.estado)
-            print("anno estudiante ",form.annoEstudiantePersona)
+            if(user["personType"] == "Student"):
+                print("anno estudiante ",form.annoEstudiantePersona)
             print("nombre ",form.nombre)
             print("rol ",form.rol)
             print("tipo de persona ",form.tipoPersona)
-            print("rol anterior",form.rol_anterior)
-            print("rol anterior",form.tipoMotivo)
+            if person != None:
+                print("rol anterior",person.rol)  
+            else:
+                print("rol anterior","No existe Rol anterior")    
+            print("motivo",form.tipoMotivo)
+            print("comprobante motivo: ",form.comprobante_motivo)
 
             print("============== Estos son los datos que se recogieron==========")
 
@@ -252,7 +309,14 @@ async def crear_carnet_post(area,ci,request:Request, db: Session = Depends(get_d
 
             else:
                 person_a = update_person_by_ci(ci, person,db)
+                form.folio_desactivo = carnet_anterior.folio
+                form.area_anterior = person.area
+                form.rol_anterior = person.rol
+                person_a = PersonCreate(**form.__dict__)
 
+                person_a = update_person_by_ci(ci, person_a,db)
+                print("Se actualizo la persona")
+                
             if carnet_anterior is not None: # si tiene carnet anterior
                 print("existia un carnet anterior y se actualizara")
                 form.folio_desactivo= carnet_anterior.folio 
